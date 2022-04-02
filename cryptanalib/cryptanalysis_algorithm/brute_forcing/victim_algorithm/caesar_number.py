a311e65f7b8090fb0a4d673215bd7008a1fd18b0
gogo <gogo246475@gmail.com>
2022-04-02 18:10:52 +0200

    Refactor method brute_force on caesar algo.

--- cryptanalib/cryptanalysis_algorithm/brute_forcing/victim_algorithm/caesar_number.py
+++ cryptanalib/cryptanalysis_algorithm/brute_forcing/victim_algorithm/caesar_number.py
@@ -5,40 +5,39 @@ from cryptanalib.encryption.caesar_number_encryption import CaesarNumberEncrypti
 from cryptanalib.encoding.alphabet import Alphabet
 
 class CaesareNumberBruteForcer(GenericBruteForcer):
-    def __init__(self, cipher_text, alphabet=Alphabet().ascii_albhabet):
+    def __init__(self, alphabet=Alphabet().ascii_albhabet):
         """
         Class to handle brute forcing on the caesar algorithm.
 
         The default alphabet used by Caesar is the ascii (127 characters). This can be changed by the parameter alphabet to this class.
 
 
-        :param cipher_text: list of int
         :param alphabet: string
         """
-        self.cipher_text = cipher_text
         self.alphabet = alphabet
 
-    def brute_force(self, format="ascii_charset", language="english", frequency_required=0.25):
+    def brute_force(self, cipher_text, format="ascii_charset", language="english", frequency_required=0.25):
         """
         Brute force any possibility of alphabets shifting with the caesar encryption algorithm.
 
         First decode the format given with the variable format.
         Then if some natural language words correspond to a dictionary of language specified by the language in the variable language, return a list of hex values of the decoded palin text.
 
+        :param cipher_text: list of int
         :param format: string
         :param language: string
         :return: array of hex values
 
         >>> from cryptanalib.cryptanalysis_algorithm.brute_forcing.victim_algorithm.caesar_number import CaesareNumberBruteForcer
         >>> from cryptanalib.encoding.alphabet import Alphabet
-        >>> cnbf = CaesareNumberBruteForcer([ord(number) for number in list("A5&AI<R!I<R!A(&=R96%T(&1A>2!T;R!D:64@=&]D87DA")])
-        >>> cnbf.brute_force(format="uu_charset", language="english", frequency_required=0.25)
+        >>> cnbf = CaesareNumberBruteForcer(alphabet=Alphabet().ascii_albhabet)
+        >>> cnbf.brute_force([ord(number) for number in list("A5&AI<R!I<R!A(&=R96%T(&1A>2!T;R!D:64@=&]D87DA")], format="uu_charset", language="english", frequency_required=0.25)
         1.0 level of similarity with the common words: ['This', 'is', 'a', 'great', 'day', 'to', 'die', 'today', '!'].
         Caesar number algorithm successfully brute forced with key 0
         'This is a great day to die today!'
 
-        >>> cnbf = CaesareNumberBruteForcer([ord(number) for number in list("<0!<D7M∟D7M∟<#!8M41 O#!,<9-∟O6M∟?51/;8!X?32?<")])
-        >>> cnbf.brute_force(format="uu_charset", language="english", frequency_required=0.75)
+        >>> cnbf = CaesareNumberBruteForcer(alphabet=Alphabet().ascii_albhabet)
+        >>> cnbf.brute_force([ord(number) for number in list("<0!<D7M∟D7M∟<#!8M41 O#!,<9-∟O6M∟?51/;8!X?32?<")], format="uu_charset", language="english", frequency_required=0.75)
         0.42857142857142855 level of similarity with the common words: ['@', '$', '$', '`', 'M', "'"].
         0.6 level of similarity with the common words: [']', '&', 'nU', '#', ']', '`', 'Y', '$', '`'].
         0.5714285714285714 level of similarity with the common words: [']', ']', 'h', '>'].
@@ -59,7 +58,7 @@ class CaesareNumberBruteForcer(GenericBruteForcer):
         if format == "uu_charset":
             caesar = CaesarNumberEncryption(self.alphabet)
             for i in range(30):
-                brute_forced_text = caesar.encrypt(self.cipher_text, i)
+                brute_forced_text = caesar.encrypt(cipher_text, i)
                 potentially_decoded_brute_forced_text = f.decoding[format]("".join([chr(text) for text in brute_forced_text]))#todo: pass hex to uu next
                 is_plain_text_found = ptd.detect_plain_text(potentially_decoded_brute_forced_text, frequency_required)
 
